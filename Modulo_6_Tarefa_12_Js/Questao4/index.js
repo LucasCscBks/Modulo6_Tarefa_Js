c331ef1e3ebb5505f1afb4d745c40304f34ee846
LucasCscBks <lucas_reisgod@yahoo.com.br>
2022-07-11 03:14:10 -0300

    tarefa 12 questao baralho

--- Modulo_6_Tarefa_12_Js/Questao4/index.js
+++ Modulo_6_Tarefa_12_Js/Questao4/index.js
@@ -1,27 +1,408 @@
 let baralho = [
     {
     'naipe': 'paus',
-    'carta': 2
+    'carta': 2,
+    'imagem': 'images/2_paus.svg'
 },
 {
     'naipe': 'paus',
-    'carta': 3
+    'carta': 3,
+    'imagem': 'images/3_paus.svg'
 },
 {
     'naipe': 'paus',
-    'carta': 4
+    'carta': 4,
+    'imagem': 'images/4_paus.svg'
 },
 {
     'naipe': 'paus',
-    'carta': 5
+    'carta': 5,
+    'imagem': 'images/5_paus.svg'
 },
 {
     'naipe': 'paus',
-    'carta': 6
+    'carta': 6,
+    'imagem': 'images/6_paus.svg'
 },
 {
     'naipe': 'paus',
-    'carta': 7
+    'carta': 7,
+    'imagem': 'images/7_paus.svg'
+},
+{
+    'naipe': 'paus',
+    'carta': 8,
+    'imagem': 'images/8_paus.svg'
+},
+{
+    'naipe': 'paus',
+    'carta': 9,
+    'imagem': 'images/9_paus.svg'
+},
+{
+    'naipe': 'paus',
+    'carta': 10,
+    'imagem': 'images/10_paus.svg'
+},
+{
+    'naipe': 'paus',
+    'carta': 11,
+    'valor': 'J',
+    'imagem': 'images/11_paus.svg'
+},
+{
+    'naipe': 'paus',
+    'carta': 12,
+    'valor': 'Q',
+    'imagem': 'images/12_paus.svg'
+},
+{
+    'naipe': 'paus',
+    'carta': 13,
+    'valor': 'K',
+    'imagem': 'images/13_paus.svg'
+},
+{
+    'naipe': 'paus',
+    'carta': 14,
+    'valor': 'A',
+    'imagem': 'images/14_paus.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 2,
+    'imagem': 'images/2_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 3,
+    'imagem': 'images/3_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 4,
+    'imagem': 'images/4_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 5,
+    'imagem': 'images/5_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 6,
+    'imagem': 'images/6_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 7,
+    'imagem': 'images/7_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 8,
+    'imagem': 'images/8_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 9,
+    'imagem': 'images/9_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 10,
+    'imagem': 'images/10_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 11,
+    'valor': 'J',
+    'imagem': 'images/11_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 12,
+    'valor': 'Q',
+    'imagem': 'images/12_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 13,
+    'valor': 'K',
+    'imagem': 'images/13_copas.svg'
+},
+{
+    'naipe': 'copas',
+    'carta': 14,
+    'valor': 'A',
+    'imagem': 'images/14_copas.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 2,
+    'imagem': 'images/2_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 3,
+    'imagem': 'images/3_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 4,
+    'imagem': 'images/4_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 5,
+    'imagem': 'images/5_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 6,
+    'imagem': 'images/6_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 7,
+    'imagem': 'images/7_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 8,
+    'imagem': 'images/8_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 9,
+    'imagem': 'images/9_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 10,
+    'imagem': 'images/10_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 11,
+    'valor': 'J',
+    'imagem': 'images/11_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 12,
+    'valor': 'Q',
+    'imagem': 'images/12_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 13,
+    'valor': 'K',
+    'imagem': 'images/13_ouros.svg'
+},
+{
+    'naipe': 'ouro',
+    'carta': 14,
+    'valor': 'A',
+    'imagem': 'images/14_ouros.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 2,
+    'imagem': 'images/2_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 3,
+    'imagem': 'images/3_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 4,
+    'imagem': 'images/4_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 5,
+    'imagem': 'images/5_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 6,
+    'imagem': 'images/6_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 7,
+    'imagem': 'images/7_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 8,
+    'imagem': 'images/8_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 9,
+    'imagem': 'images/9_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 10,
+    'imagem': 'images/10_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 11,
+    'valor': 'J',
+    'imagem': 'images/11_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 12,
+    'valor': 'Q',
+    'imagem': 'images/12_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 13,
+    'valor': 'K',
+    'imagem': 'images/13_espadas.svg'
+},
+{
+    'naipe': 'espadas',
+    'carta': 14,
+    'valor': 'A',
+    'imagem': 'images/14_espadas.svg'
 }
 ]
+let baralhoSorteado = []
+const buttonObj = document.querySelector('#button');
+const resultObj = document.querySelector('#result');
+const result2Obj = document.querySelector('#result2');
+const result3Obj = document.querySelector('#result3');
+
+buttonObj.addEventListener('click', sort);
+
 console.log(baralho)
+let cont = 0
+
+function sort() {
+    // resultObj.innerHTML = ''
+    result2Obj.innerHTML = ''
+    result3Obj.innerHTML = ''
+    for (let i = 0; i < 5; i++) {
+        let rdn = Math.floor(Math.random() * baralho.length)
+        baralhoSorteado.push(baralho[rdn])
+        baralho.splice(rdn,1);
+    }
+    console.log(baralhoSorteado)
+    console.log(baralho)
+    baralhoSorteado.sort(function(a, b) { return a.carta - b.carta})
+    // Intervalos para puxar/mostrar carta espaçado //
+    let repeat = setInterval(function() {
+        result3Obj.innerHTML += `<img src="${baralhoSorteado[cont].imagem}">`
+        cont ++
+    }, 1000)
+    function end() {
+        clearInterval(repeat)
+        cont = 0
+    }
+    setTimeout(end, 5000)
+    // Intervalos para puxar carta espaçado //
+
+    // for (let i = 0; i < 5; i++) {
+    //     resultObj.innerHTML += JSON.stringify(baralhoSorteado[i])
+    //     result3Obj.innerHTML += `<img src="${baralhoSorteado[i].imagem}">`
+    // }
+
+    setTimeout(analisys, 6000)
+    function analisys() {
+            let cont = 1
+                // Verificando 5 naipes iguais e sequencia de 5 //
+                // STRAIGTH FLUSH
+                if (baralhoSorteado[0].naipe == baralhoSorteado[1].naipe && baralhoSorteado[1].naipe == baralhoSorteado[2].naipe && baralhoSorteado[2].naipe == baralhoSorteado[3].naipe && baralhoSorteado[3].naipe == baralhoSorteado[4].naipe) {
+                    if (baralhoSorteado[0].carta + 1 == baralhoSorteado[1].carta && baralhoSorteado[1].carta + 1 == baralhoSorteado[2].carta && baralhoSorteado[2].carta + 1 == baralhoSorteado[3].carta && baralhoSorteado[3].carta + 1 == baralhoSorteado[4].carta) {
+                        console.log(`Straigth Flush`)
+                        result2Obj.innerHTML = `STRAIGTH FLUSH`      
+                    }
+                } 
+                // Verificando 5 naipes iguais e sequencia de 5 //
+
+                // QUADRA //
+                else if (baralhoSorteado[0].carta == baralhoSorteado[1].carta && baralhoSorteado[1].carta == baralhoSorteado[2].carta && baralhoSorteado[2].carta == baralhoSorteado[3].carta && baralhoSorteado[3].carta != baralhoSorteado[4].carta) {
+                        console.log(`QUADRA`)
+                        result2Obj.innerHTML = `QUADRA`
+                } 
+                else if (baralhoSorteado[1].carta == baralhoSorteado[2].carta && baralhoSorteado[2].carta == baralhoSorteado[3].carta && baralhoSorteado[3].carta == baralhoSorteado[4].carta && baralhoSorteado[0].carta != baralhoSorteado[1].carta) {
+                        console.log(`QUADRA`)
+                        result2Obj.innerHTML = `QUADRA`
+                }
+                // QUADRA //
+
+                // FULL HOUSE //
+                else if (baralhoSorteado[0].carta == baralhoSorteado[1].carta && baralhoSorteado[0].carta == baralhoSorteado[2].carta && baralhoSorteado[3].carta == baralhoSorteado[4].carta) {
+                    console.log(`FULL HOUSE`)
+                    result2Obj.innerHTML = `FULL HOUSE`
+                }
+                else if (baralhoSorteado[0].carta == baralhoSorteado[1].carta && baralhoSorteado[2].carta == baralhoSorteado[3].carta && baralhoSorteado[2].carta == baralhoSorteado[4].carta) {
+                    console.log(`FULL HOUSE`)
+                    result2Obj.innerHTML = `FULL HOUSE`
+                }
+                // FULL HOUSE //
+
+                // SEQUÊNCIA //
+                else if (baralhoSorteado[0].carta + 1 == baralhoSorteado[1].carta && baralhoSorteado[1].carta + 1 == baralhoSorteado[2].carta && baralhoSorteado[2].carta + 1 == baralhoSorteado[3].carta && baralhoSorteado[3].carta + 1 == baralhoSorteado[4].carta) {
+                        console.log(`SEQUÊNCIA`)
+                        result2Obj.innerHTML = `SEQUÊNCIA`
+                }
+                // SEQUÊNCIA //
+
+                // TRINCA //
+                else if (baralhoSorteado[0].carta == baralhoSorteado[1].carta && baralhoSorteado[0].carta == baralhoSorteado[2].carta) {
+                    console.log(`TRINCA`)
+                    result2Obj.innerHTML = `TRINCA`
+                }
+                else if (baralhoSorteado[1].carta == baralhoSorteado[2].carta && baralhoSorteado[1].carta == baralhoSorteado[3].carta) {
+                    console.log(`TRINCA`)
+                    result2Obj.innerHTML = `TRINCA`
+                }
+                else if (baralhoSorteado[2].carta == baralhoSorteado[3].carta && baralhoSorteado[2].carta == baralhoSorteado[4].carta) {
+                    console.log(`TRINCA`)
+                    result2Obj.innerHTML = `TRINCA`
+                }
+                // TRINCA //
+
+                // DOIS PARES //
+                else if (baralhoSorteado[0].carta == baralhoSorteado[1].carta && baralhoSorteado[2].carta == baralhoSorteado[3].carta && baralhoSorteado[0].carta != baralhoSorteado[2].carta && baralhoSorteado[3].carta != baralhoSorteado[4].carta) {
+                    console.log(`DOIS PARES`)
+                    result2Obj.innerHTML = `DOIS PARES`
+                }
+                else if (baralhoSorteado[1].carta == baralhoSorteado[2].carta && baralhoSorteado[3].carta == baralhoSorteado[4].carta && baralhoSorteado[0].carta != baralhoSorteado[1].carta && baralhoSorteado[1].carta != baralhoSorteado[4].carta) {
+                    console.log(`DOIS PARES`)
+                    result2Obj.innerHTML = `DOIS PARES`
+                }
+                // DOIS PARES //
+
+                // PAR //
+                else if (baralhoSorteado[0].carta == baralhoSorteado[1].carta || baralhoSorteado[1].carta == baralhoSorteado[2].carta || baralhoSorteado[2].carta == baralhoSorteado[3].carta || baralhoSorteado[3].carta == baralhoSorteado[4].carta) {
+                    console.log(`PAR`)
+                    result2Obj.innerHTML = `PAR`
+                }
+                // PAR //
+                else {
+                    console.log(`NADA`)
+                    result2Obj.innerHTML = `NADA`
+                }
+            
+            // Resetando o Array Baralho e o Array sorteado para poder sortear quantas vezes quiser //
+            for (let i = 0; i < 5; i++) {
+                baralho.push(baralhoSorteado[i]);
+            }
+            baralhoSorteado.splice(0,5)
+            // Resetando o Array Baralho e o Array sorteado para poder sortear quantas vezes quiser //
+        console.log(result2Obj.innerText)
+        
+    }
+    
+}
