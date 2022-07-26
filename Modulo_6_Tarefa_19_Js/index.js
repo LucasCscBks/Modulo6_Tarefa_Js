71bc2a0b54be827c5baeaf953557dddb04ecb157
LucasCscBks <lucas_reisgod@yahoo.com.br>
2022-07-25 22:20:55 -0300

    update

--- Modulo_6_Tarefa_19_Js/index.js
+++ Modulo_6_Tarefa_19_Js/index.js
@@ -1,84 +1,84 @@
 const button = document.querySelector('#test');
 const sortContainer = document.querySelector('#sort_container');
 
-let section1 = document.querySelector('#cartela01');
-let section2 = document.querySelector('#cartela02');
-let section3 = document.querySelector('#cartela03');
+let section1 = document.querySelector('#card01');
+let section2 = document.querySelector('#card02');
+let section3 = document.querySelector('#card03');
 let bingoC = document.querySelector('#bingo');
 
-button.addEventListener('click', sorteador)
+button.addEventListener('click', raffle)
 
 function bingo() {
-    let arrayCartela = []
-    let sortedn = Math.ceil(Math.random() * 75)
-    function cartela() {
+    let arrayCard = []
+    let sortedN = Math.ceil(Math.random() * 75)
+    function card() {
         for (let i = 0; i < 10; i++) {
             console.log('x')
-            while (arrayCartela.includes(sortedn) === true) {
-                sortedn = Math.ceil(Math.random() * 75)
+            while (arrayCard.includes(sortedN) === true) {
+                sortedN = Math.ceil(Math.random() * 75)
             }
-            arrayCartela.push(sortedn)
+            arrayCard.push(sortedN)
         }
-        return arrayCartela.sort(function (a,b) {return a - b}) 
+        return arrayCard.sort(function (a,b) {return a - b}) 
     }
-    return cartela() 
+    return card() 
 }
 
-function sorteador() {
+function raffle() {
     button.style.display = 'none'
     sortContainer.innerHTML = ''
-    let cartela1 = bingo()
-    let cartela2 = bingo()
-    let cartela3 = bingo()
+    let card1 = bingo()
+    let card2 = bingo()
+    let card3 = bingo()
 
-    // Cartelas //
-    cartela01()
-    cartela02()
-    cartela03()
+    // cards //
+    card01()
+    card02()
+    card03()
 
-    function cartela01() {
+    function card01() {
         section1.innerHTML = ''
-        section1.innerHTML = `<h1>CARTELA 1</h1>`
-        let fullCartela = []
+        section1.innerHTML = `<h1>card 1</h1>`
+        let fullcard = []
         for (let i = 0; i < 10; i++) {
-            section1.innerHTML += `<p class="cartela1">${cartela1[i]}</p>`
+            section1.innerHTML += `<p class="card1">${card1[i]}</p>`
         }
-        let cartelaALL1 = document.querySelectorAll('.cartela1')
+        let cardALL1 = document.querySelectorAll('.card1')
         for (let i = 0; i < 10; i++) {
-            cartelaALL1[i].addEventListener('click', function() {
-                console.log(cartelaALL1[i].textContent)
-                if (sorteados.includes(Number(cartelaALL1[i].textContent)) === true) {
-                    cartelaALL1[i].style.backgroundColor = 'Green'
-                    if (fullCartela.includes(cartelaALL1[i].textContent) === false) {
-                        fullCartela.push(cartelaALL1[i].textContent)
-                        if (fullCartela.length == 10) {
+            cardALL1[i].addEventListener('click', function() {
+                console.log(cardALL1[i].textContent)
+                if (drawnArray.includes(Number(cardALL1[i].textContent)) === true) {
+                    cardALL1[i].style.backgroundColor = 'Green'
+                    if (fullcard.includes(cardALL1[i].textContent) === false) {
+                        fullcard.push(cardALL1[i].textContent)
+                        if (fullcard.length == 10) {
                             console.log('JOGADOR 1 VENCEU')
                             sortContainer.innerHTML = `<p id="sortedNumber">JOGADOR 1 VENCEU</p>`
                             clearInterval(myInterval , 5000)
                         }
                     }  
                 }
-                // console.log(fullCartela)
+                // console.log(fullcard)
             })
         }
     }
 
-    function cartela02() {
+    function card02() {
         section2.innerText = ''
-        section2.innerHTML = `<h1>CARTELA 2</h1>`
-        let fullCartela = []
+        section2.innerHTML = `<h1>card 2</h1>`
+        let fullcard = []
         for (let i = 0; i < 10; i++) {
-            section2.innerHTML += `<p class="cartela2">${cartela2[i]}</p>`
+            section2.innerHTML += `<p class="card2">${card2[i]}</p>`
         }
-        let cartelaALL2 = document.querySelectorAll('.cartela2')
+        let cardALL2 = document.querySelectorAll('.card2')
         for (let i = 0; i < 10; i++) {
-            cartelaALL2[i].addEventListener('click', function() {
-                console.log(cartelaALL2[i].textContent)
-                if (sorteados.includes(Number(cartelaALL2[i].textContent)) === true) {
-                    cartelaALL2[i].style.backgroundColor = 'Green'
-                    if (fullCartela.includes(cartelaALL2[i].textContent) === false) {
-                        fullCartela.push(cartelaALL2[i].textContent)
-                        if (fullCartela.length == 10) {
+            cardALL2[i].addEventListener('click', function() {
+                console.log(cardALL2[i].textContent)
+                if (drawnArray.includes(Number(cardALL2[i].textContent)) === true) {
+                    cardALL2[i].style.backgroundColor = 'Green'
+                    if (fullcard.includes(cardALL2[i].textContent) === false) {
+                        fullcard.push(cardALL2[i].textContent)
+                        if (fullcard.length == 10) {
                             sortContainer.innerHTML = `<p id="sortedNumber">JOGADOR 2 VENCEU</p>`
                             console.log('JOGADOR 2 VENCEU')
                             clearInterval(myInterval , 5000)
@@ -88,84 +88,85 @@ function sorteador() {
             })
         }
     }
-    function cartela03() {
+    function card03() {
         section3.innerHTML = ''
-        section3.innerHTML = `<h1>CARTELA 3</h1>`
-        let fullCartela = []
+        section3.innerHTML = `<h1>card 3</h1>`
+        let fullcard = []
         for (let i = 0; i < 10; i++) {
-            section3.innerHTML += `<p class="cartela3">${cartela3[i]}</p>`
+            section3.innerHTML += `<p class="card3">${card3[i]}</p>`
         }
-        let cartelaALL3 = document.querySelectorAll('.cartela3')
+        let cardALL3 = document.querySelectorAll('.card3')
         for (let i = 0; i < 10; i++) {
-            cartelaALL3[i].addEventListener('click', function() {
-                console.log(cartelaALL3[i].textContent)
-                if (sorteados.includes(Number(cartelaALL3[i].textContent)) === true) {
-                    cartelaALL3[i].style.backgroundColor = 'Green'
-                    if (fullCartela.includes(cartelaALL3[i].textContent) === false) {
-                        fullCartela.push(cartelaALL3[i].textContent)
-                        if (fullCartela.length == 10) {
+            cardALL3[i].addEventListener('click', function() {
+                console.log(cardALL3[i].textContent)
+                if (drawnArray.includes(Number(cardALL3[i].textContent)) === true) {
+                    cardALL3[i].style.backgroundColor = 'Green'
+                    if (fullcard.includes(cardALL3[i].textContent) === false) {
+                        fullcard.push(cardALL3[i].textContent)
+                        if (fullcard.length == 10) {
                             sortContainer.innerHTML = `<p id="sortedNumber">JOGADOR 3 VENCEU</p>`
                             console.log('JOGADOR 3 VENCEU')
                             clearInterval(myInterval , 5000)
                         }
                     }  
                 }
-                // console.log(fullCartela)
+                // console.log(fullcard)
             })
         }
     }
-    // Cartelas //
-    console.log(cartela1)
-    console.log(cartela2)
-    console.log(cartela3)
+    // cards //
+    console.log(card1)
+    console.log(card2)
+    console.log(card3)
 
-    let arraySorteador = []
-    let sorteados = []
-    // let numSorteado = 0
+    let arrayRaffle = []
+    let drawnArray = []
+    // let numDrawn = 0
 
-    function criandoArraySorteador(min,max) {
+    function createArrayRaffle(min,max) {
     //    num = Math.floor(Math.random() * (max - min)) + min
         for (let i = min; i <= max; i++) {
-            arraySorteador.push(i);
+            arrayRaffle.push(i);
         }
-        // console.log(arraySorteador)
-        return arraySorteador
+        // console.log(arrayRaffle)
+        return arrayRaffle
     }
-    criandoArraySorteador(1,75)
-    console.log(arraySorteador)
+    createArrayRaffle(1,75)
+    console.log(arrayRaffle)
 
-    sorteadorArray()
-    function sorteadorArray() {
+    raffleArray()
+    function raffleArray() {
         for (let i = 0; i < 75; i ++) {
-            bingoC.innerHTML += `<p class="sorteadorArray">${arraySorteador[i]}</p>`
+            bingoC.innerHTML += `<p class="raffleArray">${arrayRaffle[i]}</p>`
         }  
     }
-    let AllSorteador = document.querySelectorAll('.sorteadorArray');
-    console.log(AllSorteador)
+    let allRaffle = document.querySelectorAll('.raffleArray');
+    console.log(allRaffle)
 
-    let numSorteado = arraySorteador[Math.floor(Math.random() * arraySorteador.length)]
-    function sortear() {
-        
-        function numberSorteado() {
-            numSorteado = arraySorteador[Math.floor(Math.random() * arraySorteador.length)]
-            console.log(numSorteado)
-            return numSorteado
-        }
-        // numberSorteado()
-        
-        while (sorteados.includes(numSorteado) === true) {
-            numberSorteado()
-        }
-        sortContainer.innerHTML = `<p id="sortedNumber">${numSorteado}</p>`
-        AllSorteador[numSorteado - 1].style.backgroundColor = 'Yellow'
-        sorteados.push(numSorteado)
-        
-        console.log(sorteados)
+    let numDrawn = arrayRaffle[Math.floor(Math.random() * arrayRaffle.length)]
 
-        if (sorteados.length === arraySorteador.length) {
+    function drawn() {
+        
+        function numberDrawn() {
+            numDrawn = arrayRaffle[Math.floor(Math.random() * arrayRaffle.length)]
+            console.log(numDrawn)
+            return numDrawn
+        }
+        // numberDrawn()
+        
+        while (drawnArray.includes(numDrawn) === true) {
+            numberDrawn()
+        }
+        sortContainer.innerHTML = `<p id="sortedNumber">${numDrawn}</p>`
+        allRaffle[numDrawn - 1].style.backgroundColor = 'Yellow'
+        drawnArray.push(numDrawn)
+        
+        console.log(drawnArray)
+
+        if (drawnArray.length === arrayRaffle.length) {
             clearInterval(myInterval , 5000)
         }
     }
-    let myInterval = setInterval(sortear, 5000)
+    let myInterval = setInterval(drawn, 5000)
 }
-// console.log(sorteador())
+// console.log(raffle())
